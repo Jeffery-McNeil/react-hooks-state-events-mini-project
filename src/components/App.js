7bbb286ad25458b0b75f2f1aac7c1c5943398f48
Jeffery <jeffmc825@gmail.com>
2021-12-19 10:22:00 -0600

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,31 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS)
+  const [category, setCategory] = useState("All")
+
+  function handleDeleteTask(deletedTaskText) {
+    setTasks(tasks.filter((task) => task.text !== deletedTaskText));
+  }
+
+  function handleCategoryChange(newCategory) {
+    setCategory(newCategory)
+  }
+
+  function handleAddTask(newTask) {
+    setTasks([...tasks, newTask])
+  }
+
+  const visibleTasks = tasks.filter(
+    (task) => category === "All" || task.category === category
+  );
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter onSelectCategory={handleCategoryChange} selectedCategory={category} categories={CATEGORIES} />
+      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={handleAddTask}/>
+      <TaskList onDeleteTask={handleDeleteTask} tasks={visibleTasks} />
     </div>
   );
 }
