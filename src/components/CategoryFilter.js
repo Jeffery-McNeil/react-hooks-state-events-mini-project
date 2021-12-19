532287da4354b7462172157467d4cb9a4a4a0f65
Jeffery <jeffmc825@gmail.com>
2021-12-19 10:21:35 -0600

    commit tree c585e5be5e70f650632269cf08385d4b982fb194

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,28 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ onSelectCategory, selectedCategory, categories }) {
+  const categoryButtons = categories.map((category) => {
+    const className = category === selectedCategory ? "selected" : null
+  
+  return (
+    <button 
+      key={category}
+      className={className}
+      onClick={()=> onSelectCategory(category)}
+    >
+      {category}
+    </button>
+  );
+  });
+
+
   return (
     <div className="categories">
-      <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      <h5>Category Filters</h5>
+      {categoryButtons}
     </div>
   );
+
 }
 
 export default CategoryFilter;
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState("Code");
+  
+  
+  const renderOptions = categories.map((category)=> {
+    if (category === "All") {
+      return null
+    } else {
+      return <option key={category}>{category}</option>
+    }
+  })
+  
+  function handleSubmit(event) {
+    event.preventDefault()
+    onTaskFormSubmit({ text, category })
+    setText("")
+    setCategory("Code")
+  }
+
+  
   return (
-    <form className="new-task-form">
+    <form onSubmit={handleSubmit} className="new-task-form">
       <label>
         Details
-        <input type="text" name="text" />
+        <input 
+          type="text" 
+          name="text" 
+          value={text}
+          onChange={(event) => setText(event.target.value)}
+        />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select 
+        name="category"
+        value={category}
+        onChange={(event)=> setCategory(event.target.value)}
+        >
+          {renderOptions}
         </select>
       </label>
       <input type="submit" value="Add task" />
